63476fd8bddb47b1c34f939d7e33bd733321d22d
Boris Mihajlovic <bomi2006@enjoying.rs>
2020-06-21 22:26:39 +0200

    FirstCommit

---
+++ HotelReservation/HotelReservations/HotelReservations/Hotel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HotelReservations.Room;
+
+namespace HotelReservations.Room
+{
+    public class Hotel
+    {
+        /*key represent number of room, value represent Room entity*/
+        public Dictionary<int, Room> rooms;
+        public Hotel(int size)
+        {
+            if (size <= 1000)
+            {
+                rooms = new Dictionary<int, Room>();
+                for (int i = 1; i <= size; i++)
+                {
+                    rooms.Add(i, new Room());
+                }
+                foreach (var room in rooms)
+                {
+                    int j = 0;
+                    while (j < 365)
+                    {
+                        room.Value.dayStatus.Add(j, true);
+                        j++;
+                    }
+                }
+            }
+        }
+        public void RoomReservation(int startDay, int endDay)
+        {
+            bool reservationStatus = false;
+            if (startDay >= 0 && endDay - startDay >= 0 && endDay < 365)
+            {
+                RoomSorting();/* Sorting by most optimal room for reservation */
+                foreach (var room in rooms)
+                {
+                    var roomReservation = true;
+                    for (int i = startDay; i <= endDay; i++)
+                    {
+                        if (room.Value.dayStatus[i] == false)
+                        {
+                            roomReservation = false;
+                            break;
+                        }
+                    }
+                    if (roomReservation == true)
+                    {
+                        room.Value.Reservation(startDay, endDay);
+                        reservationStatus = true;
+                        break;
+                    }
+                }
+                if (reservationStatus == true)
+                {
+                    Console.WriteLine("StartDate " + startDay + " EndDate " + endDay + " Accept");
+                }
+                else
+                {
+                    Console.WriteLine("StartDate " + startDay + " EndDate " + endDay + " Decline");
+                }
+            }
+            else
+                Console.WriteLine("StartDate " + startDay + " EndDate " + endDay + " Decline");
+        }
+        public void RoomSorting()
+        {
+            var firstRoom = rooms.ElementAt(0);
+            for (int i = 1; i < rooms.Count; i++)
+            {
+                var room = rooms.ElementAt(i);
+                if (firstRoom.Value.dayfree < room.Value.dayfree)
+                {
+                    var pom = rooms[1];
+                    rooms[1] = rooms[i+1];
+                    rooms[i+1] = pom;
+                }
+            }
+        }
+    }
+}
---
+++ HotelReservation/HotelReservations/HotelReservations/Room/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservations.Room
+{
+    public class Room
+    {
+        /*In dayStatus dictionary key represent day, value represent is day reserved or not*/
+        public Dictionary<int,bool> dayStatus;
+        /* dayfree represent which day is available for*/
+        public int dayfree = 0;
+        public Room()
+        {
+            dayStatus = new Dictionary<int, bool>();
+        }
+        public void Reservation(int startDay, int endDay)
+        {
+            for(int i= startDay; i <= endDay; i++)
+            {
+                dayStatus[i] = false;
+            }
+            dayfree = endDay + 1;
+        }
+    }    
+}
+
